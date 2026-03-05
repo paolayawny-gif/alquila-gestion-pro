d8adc975dfaff10285656f32270f00069db53e01
Nicolás Morcillo <nicolasmmorcillo@gmail.com>
2026-03-05 10:55:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2174718446-d2df4",
+  "appId": "1:756848331173:web:4787898f0eb17b0ffa004c",
+  "apiKey": "AIzaSyDqR9jZmajeUU8Z6FWf84kse2beVNsvzVE",
+  "authDomain": "studio-2174718446-d2df4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "756848331173"
+};
